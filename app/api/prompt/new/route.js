b5542e749ebccb3da4fe7ec153d76f4585f78a7a
SidxRengen <gautamsiddharth226@gmail.com>
2023-08-29 22:34:22 +0530

    changes

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -19,7 +19,7 @@ export const POST = async (request) => {
   const { userID, prompt, tag } = await request.json();
   try {
     await connectToDB();
-    const newPrompt = new Prompt({ creator: userID, prompt, tag });
+    const newPrompt = new Prompt({creator: userID, prompt, tag });
 
     await newPrompt.save();
     return new Response(JSON.stringify(newPrompt), { status: 201 });
