f3735b04b40318b6114f7ff3d0e64a3ea480a79d
SidxRengen <gautamsiddharth226@gmail.com>
2023-08-30 15:18:49 +0530

    changes

--- app/api/prompt/searchPrompt/route.js
+++ app/api/prompt/searchPrompt/route.js
@@ -6,18 +6,27 @@ export const POST = async (request) => {
 
   try {
     await connectToDB();
-    const prompts = await Prompt.find({ tag: search }).populate("creator");
+    const prompts = (await Prompt.find().populate("creator")).filter(
+      (prompt) => {
+        return (
+          prompt.tag.slice(0, search.length) === search ||
+          prompt.prompt.slice(0, search.length) === search
+        );
+      }
+    );
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
 export const PUT = async (request) => {
-  const { search , id} = await request.json();
+  const { search, id } = await request.json();
 
   try {
     await connectToDB();
-    const prompts = await Prompt.find({ tag: search,creator:id }).populate("creator");
+    const prompts = await Prompt.find({ tag: search, creator: id }).populate(
+      "creator"
+    );
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
